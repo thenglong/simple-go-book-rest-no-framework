2b6f6f52afa4856d67d7b678c3359bc5aa8fc5b1
long <long@long.com>
2021-05-03 06:30:21 +0700

    v1.1

--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import (
 	"boot-rest-api/handlers"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	var bh handlers.BookHandler
 
 	http.Handle("/books", &bh)
@@ -14,5 +20,5 @@ func main() {
 	// this handles the /book/id
 	http.Handle("/books/", &bh)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
